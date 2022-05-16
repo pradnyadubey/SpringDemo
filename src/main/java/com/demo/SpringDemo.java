193e0e823e018f553048ab1dbfed7a4fedb05730
pthakurdas <77017610+pthakurdas@users.noreply.github.com>
2022-05-16 15:20:07 +0530

    test

---
+++ src/main/java/com/demo/SpringDemo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo.class, args);
+	}
+}
