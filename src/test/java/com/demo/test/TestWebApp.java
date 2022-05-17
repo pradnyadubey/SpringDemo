e17f1bb46881929658c0ee9422bf6018cb8465ff
pthakurdas <77017610+pthakurdas@users.noreply.github.com>
2022-05-17 11:26:45 +0530

    test

--- src/test/java/com/demo/test/TestWebApp.java
+++ src/test/java/com/demo/test/TestWebApp.java
@@ -8,15 +8,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TestWebApp extends SpringDemo {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
+	@Autowired
 	private MockMvc mockMvc;
 
 	@Before
